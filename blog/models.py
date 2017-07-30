48a89a12ddc355ba7d27820f9f4f1e821a332533
AimeeGao <314552015@qq.com>
2017-07-30 18:47:32 +0800

    add comment

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=300)
+    title = models.CharField("title",max_length=300)
     body = models.TextField(default='')
     author = models.ForeignKey(User,related_name="blog_posts")
     publish = models.DateTimeField(default=timezone.now)
-
 
 
